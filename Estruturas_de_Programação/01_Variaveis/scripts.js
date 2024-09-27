0ac9e12483029e871e90fa00ed5c58afba3bfb82
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-09-26 22:06:00 -0300

    :lipstick: feat: Variable basics

---
+++ Estruturas_de_Programação/01_Variaveis/scripts.js
@@ -0,0 +1,20 @@
+let name = "Lindembergue";
+
+console.log("Hello, " + name + "!");
+
+name = "José";
+console.log("Hello, " + name + "!");
+
+let quantity = 6;
+
+console.log("The quantity is " + quantity);
+
+quantity = 10-quantity;
+console.log(quantity);
+
+quantity = "Agora sou string";
+
+console.log(quantity);
+
+let a = 3, b = 5, c = 2;
+console.log(a + b + c);
