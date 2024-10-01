5486163734eda9db9a5c757e79fc140a7a681d66
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-01 15:18:59 -0300

    feat: switch case

---
+++ Estruturas_de_Programação/04-switch-case/scripts.js
@@ -0,0 +1,13 @@
+let name = "testA";
+
+switch (name) {
+    case "testA":
+        console.log("Nome é testA");
+        break;
+    case "testB":
+        console.log("Nome é testB");
+        break;
+    default:
+        console.log("Nome não encontrado");
+        break;
+}
