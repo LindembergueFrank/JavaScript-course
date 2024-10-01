ebd9d6267ef078a5787b85a05972f6b26752edfb
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-01 13:06:50 -0300

    :lipstick: feat: else if

---
+++ Estruturas_de_Programação/02-condicionais/scripts.js
@@ -0,0 +1,9 @@
+let name = "NomeRandom";
+
+if (name == "bruno"){
+    console.log("Seu nome é Bruno!");
+} else if (name == "joao") {
+    console.log("Seu nome é João!");
+} else {
+    console.log("Nome desconhecido!");
+}
