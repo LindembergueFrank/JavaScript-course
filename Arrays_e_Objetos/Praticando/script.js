d8535d46097b57a9c9e42b6f829ab699d4acdd8f
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-07 15:05:07 -0300

    feat: more pratice

--- Arrays_e_Objetos/Praticando/script.js
+++ Arrays_e_Objetos/Praticando/script.js
@@ -10,8 +10,8 @@ console.log("\nQuestion 2");
 let array2 = [1, 2];
 let array3 = [3, 4, 5, 6];
 
-console.log("Array 2 tem " + array2.length + " elementos");
-console.log("Array 3 tem " + array3.length + " elementos");
+console.log("Array2 tem " + array2.length + " elementos");
+console.log("Array3 tem " + array3.length + " elementos");
 
 console.log("\nQuestion 3");
 
@@ -41,3 +41,75 @@ for(let i = 0; i < nomes.length; i++) {
         break;
     }
 }
+
+console.log("\nQuestion 6");
+let array5 = [10, 11, 12, 13, 14];
+
+function quantidadeDeElementos(arrayTest) {
+    if (arrayTest.length >= 5) {
+        console.log("Muitos elementos");
+    } else {
+        console.log("Poucos elementos");
+    }
+}
+
+console.log("Array5: ");
+quantidadeDeElementos(array5);
+console.log("Array3: ");
+quantidadeDeElementos(array3);
+
+console.log("\nQuestion 7");
+console.log("Imprimindo por iteração: ");
+for(var i=0; i<array5.length; i++) {
+    console.log(array5[i]);
+}
+
+console.log("\nQuestion 8");
+console.log("Criando um JSON com 4 proriedades");
+
+let pessoa = {
+    "name": "Marcos",
+    "email": "marcoide19@gmail.com",
+    "idade": 25,
+    "languages": ["C#", "Java", "JavaScript", "Python"]
+};
+
+console.log(pessoa.name + ", " + pessoa.email + ", " + pessoa.idade + ", " + pessoa.languages);
+
+console.log("\nQuestion 9");
+let string = "String que sera dividida por palavras e guardadas em um array";
+console.log(string);
+
+let arrayString = string.split(" ");
+
+console.log("\nArray com palavras da string: ");
+for (let i = 0; i <arrayString.length; i++) {
+    console.log(arrayString[i]);
+}
+
+console.log("\nQuestion 10");
+
+let calculadora = {
+    soma: function(numero1, numero2) {
+        return numero1 + numero2;
+    },
+    subtracao: function(numero1, numero2) {
+        return numero1 - numero2;
+    },
+    multiplicar: function(numero1, numero2) {
+        return numero1 * numero2;
+    },
+    dividir: function(numero1, numero2) {
+        if(numero2!= 0) {
+            return numero1 / numero2;
+        } else {
+            return "Não é possível realizar divisão por zero";
+        }
+    }
+}
+
+console.log("Soma de 5 + 3: " + calculadora.soma(5, 3));
+console.log("Subtracao de 5 - 3: " + calculadora.subtracao(5, 3));
+console.log("Multiplicacao de 5 x 3: " + calculadora.multiplicar(5, 3));
+console.log("Divisao de 10 / 2: " + calculadora.dividir(10, 2));
+console.log("Divisao de 10 / 0: " + calculadora.dividir(10, 0));
