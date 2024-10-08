dc8e6f6adfbdb67efb4756c1ffd5fed110a3f0d1
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-08 15:43:59 -0300

    feat: method isArray

---
+++ Arrays_e_Objetos/Métodos/Array.isArray()/isArray.js
@@ -0,0 +1,6 @@
+// Método genérico que permite verificar se é um array (true: false)
+let string = "string";
+let array = ["string", "characters", "array"];
+
+console.log(`Verificando se [${string}] é um array: ` + Array.isArray(string));
+console.log(`Verificando se [${array}] é um array: ` + Array.isArray(array));
