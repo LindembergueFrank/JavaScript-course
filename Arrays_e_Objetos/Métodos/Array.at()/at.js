5b9550e79d89244fca76958b0125a7275cc6cdcc
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-08 16:53:39 -0300

    feat: method Array.at

---
+++ Arrays_e_Objetos/Métodos/Array.at()/at.js
@@ -0,0 +1,7 @@
+// Método que permite criar uma nova instancia de array com os argumentos passados
+
+console.log("Criando array com construtor: e parametros (1, 2, 3), array: [" + Array(1, 2, 3) + "] Tamanho: " + Array(1, 2, 3).length);
+console.log("Criando array com método 'of()' e parametros (1, 2, 3) array: : [" + Array.of(1, 2, 3) + "] Tamanho: " + Array.of(1, 2, 3).length);
+console.log('\n------------------------');
+console.log("\nCriando array com construtor e parametro 7: [" + Array(7) + "] Tamanho: " + Array(7).length);
+console.log("Criando array com método 'of() e parametro 7': [" + Array.of(7) + "] Tamanho: " + Array.of(7).length);
