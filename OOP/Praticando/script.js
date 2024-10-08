35fc21c7378d0ddf8663fda725a0e037fd80a680
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-08 12:31:54 -0300

    feat: more pratice

--- OOP/Praticando/script.js
+++ OOP/Praticando/script.js
@@ -27,3 +27,74 @@ console.log(conta1);
 
 conta1.depositar(500);
 conta1.saque(1000);
+
+console.log("\nQuestion 2");
+
+class carrinhoDeCompras {
+    constructor(itens, quantidadeTotal, valorTotal) {
+        this.itens = itens;
+        this.quantidadeTotal = quantidadeTotal;
+        this.valorTotal = valorTotal;
+    }
+    
+    adicionarItem(itens, quantidade, valor) {
+
+        for(let i=0; i<quantidade; i++) {
+            this.itens.push(itens);
+            this.quantidade++;
+        }
+
+        this.valorTotal += valor;
+        this.quantidadeTotal += quantidade;
+    }
+
+    removerItem(itens, quantidade, valor) {
+        for(let i=0; i<quantidade; i++) {
+            if(itens == this.itens[i]) {
+                this.itens.splice(i, 1);
+                this.quantidade--;
+                break;
+            }
+        }
+
+        this.valorTotal -= valor;        
+    }
+}
+
+let carrinho = new carrinhoDeCompras([
+    {
+    codigo: 1,
+    nome: "Camiseta",
+    quantidade: 2,
+    preco: 150
+    } ,
+    {
+    codigo: 2,
+    nome: "Calça",
+    quantidade: 2,
+    preco: 200
+    }
+], (4), (700));
+
+
+console.log(carrinho);
+
+let itensAdicionados = [
+    {
+        codigo: 3,
+        nome: "Bermuda",
+        quantidade: 1,
+        preco: 120
+    }, 
+    {
+        codigo: 4,
+        nome: "Tênis",
+        quantidade: 1,
+        preco: 180
+    }
+]
+
+carrinho.removerItem(itensAdicionados, 2, 300);
+console.log("----------------------------");
+carrinho.adicionarItem(itensAdicionados, 2, 300);
+console.log(carrinho);
