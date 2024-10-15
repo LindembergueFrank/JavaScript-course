78ea89081a3cae4cf076817d5f994296cd680d79
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-15 20:24:53 -0300

    feat: pratice and pratice

--- Expressões regulares/Praticando/regex.js
+++ Expressões regulares/Praticando/regex.js
@@ -14,3 +14,30 @@ console.log(terminaComId.test("nomeID"));
 console.log(terminaComId.test("nome"));
 console.log(terminaComId.test("IDnome"));
 console.log(terminaComId.test("1212ID"));
+
+console.log("\nRegex utilizando '|'");
+
+const validaMarca = /Marca: (Nike|Adidas|Asisd|Lacoste|Puma)/;
+
+console.log(validaMarca.test("Marca: Nike"));
+console.log(validaMarca.test("Marca: Adidass"));
+console.log(validaMarca.test("Marca: Kappa"));
+console.log(validaMarca.test("asascasdNIkasE"));
+
+console.log("\nRegex que valida um IP, exemplo: 127.0.0.1");
+
+const validaIp = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+
+console.log(validaIp.test("127.0.0.1"));
+console.log(validaIp.test("127.0.0.1.1"));
+ console.log(validaIp.test("1246.0.0.1"));
+ console.log(validaIp.test("127001"));
+
+ console.log("\nRegex que valida um usuario no sistema");
+
+ const validaUsuario = /^(?=.{3,16}$)[a-z0-9-_]/;
+
+ console.log(validaUsuario.test("user123"));
+ console.log(validaUsuario.test("teasae@example.com"));
+ console.log(validaUsuario.test("us_-"));
+ console.log(validaUsuario.test("1111111111111111111111111"));
