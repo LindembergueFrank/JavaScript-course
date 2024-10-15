e13ae51166bb402e981151cb0e12e3fd5400c300
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-15 19:33:44 -0300

    feat: search strings with regex

--- Expressões regulares/Praticando/regex.js
+++ Expressões regulares/Praticando/regex.js
@@ -5,3 +5,12 @@ const apenasMaisculas = /[A-Z]/;
 console.log(apenasMaisculas.test("ABCDE"));
 console.log(apenasMaisculas.test("abcde"));
 console.log(apenasMaisculas.test("ADASbasasas"));
+
+console.log("\nRegex que só aceita strings terminadas com 'ID'");
+
+const terminaComId = /ID$/;
+
+console.log(terminaComId.test("nomeID"));
+console.log(terminaComId.test("nome"));
+console.log(terminaComId.test("IDnome"));
+console.log(terminaComId.test("1212ID"));
