e7ce25e70d5ff0e2926bb8ddb2a0dc38432dd185
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-22 16:17:32 -0300

    feat: event about focus

---
+++ Events/event-focus/js/script.js
@@ -0,0 +1,9 @@
+let input = document.querySelector('#input');
+
+input.addEventListener("focus", () => {
+    console.log("Entrou no input");
+});
+
+input.addEventListener("blur", () => {
+    console.log("Saiu do input");
+});
