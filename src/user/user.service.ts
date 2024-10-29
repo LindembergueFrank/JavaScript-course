cada9cf78a3ee6d19c14b30694997181a21f60d9
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-10-28 22:37:46 -0300

    feat: encryption password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) {}
 
     async createUser(data: Prisma.UserCreateInput): Promise<User> {
-        return this.prisma.user.create({
-          data,
+        const hashPassword = await bcrypt.hash(data.password, 15);
+        
+        return this.prisma.user.create({ 
+            data: {...data, password: hashPassword}, 
         });
       }
 
